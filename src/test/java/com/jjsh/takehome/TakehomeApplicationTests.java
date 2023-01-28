9ffcea983f6d9e8e0610a2202bc7fb2b0ceee85d
Jairo Sanchez <jairojsh@gmail.com>
2023-01-28 13:43:54 -0600

    Initial commit

---
+++ src/test/java/com/jjsh/takehome/TakehomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jjsh.takehome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TakehomeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
